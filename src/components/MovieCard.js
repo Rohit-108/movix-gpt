5c42f991c422603d2ddcbc17cecad07474f481e5
Rohit Kumar <rohitsharma1082001@gmail.com>
2025-07-25 23:59:18 +0530

    add some details in card

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,12 +1,21 @@
 import React from 'react'
 import { IMG_CDN_URL } from '../utils/constant'
 
-const MovieCard = ({ posterPath }) => {
+const MovieCard = ({ movie }) => {
+
+
+    const { original_title, overview, popularity, poster_path } = movie;
 
 
     return (
         <div className='w-48 pr-4'>
-            <img alt="Movie Poster" src={IMG_CDN_URL + posterPath} />
+            <img alt="Movie Poster" src={IMG_CDN_URL + poster_path} />
+            <div>
+                <div>{original_title}</div>
+                <div>{overview}</div>
+                <div>{popularity}</div>
+                <div></div>
+            </div>
         </div>
     )
 }
--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -4,14 +4,13 @@ import MovieCard from './MovieCard'
 
 const MovieList = ({ title, movies }) => {
 
-    console.log(movies)
     return (
         <div className='px-6 '>
             <h1 className='text-3xl py-4 font-bold text-white'>{title}</h1>
             <div className='flex gap-4  pb-4 overflow-x-auto overflow-y-hidden scrollbar-hide'>
                 <div className='flex'>
                     {movies?.map((movie) => (
-                        <MovieCard key={movie.id} posterPath={movie.poster_path} />
+                        <MovieCard key={movie.id} movies={movie} />
                     ))}
                 </div>
             </div>
